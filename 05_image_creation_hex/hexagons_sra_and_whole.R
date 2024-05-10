6e99cce1c03e37a7fb2b1929f7e095c1b5cec642
Alexa <astyszka@gmail.com>
2024-05-09 19:26:14 -0500

    Moving whole hex plot to the same file as sra

--- 05_image_creation_hex/hexagons_sra_and_whole.R
+++ 05_image_creation_hex/hexagons_sra_and_whole.R
@@ -19,11 +19,13 @@ library(svglite)
 # for R < 4, since H3 addresses are handled as strings
 options(stringsAsFactors = FALSE)
 setwd("~")
-sra <- read.csv(file ="~/../../scratch/alexa/pres_rev_figs/01_data/03_cleaned_coords_for_maps/coords_sradb.csv")
+sra <- read.csv(file ="~/Documents/GitHub/preservation-review-2024/03_compile_species_observations/output_orzya_example_checked_sra_names_in_results.csv")
+colnames(sra)
+colnames(sra)[4] <- "scrubbed_species_binomial"
 just_coords_sra <- sra %>% dplyr::select(latitude, longitude, scrubbed_species_binomial)
 ###remove NA values from lat/long data.
-#just_coords_sra <- just_coords_sra[!is.na(just_coords_sra$latitude),]
-#just_coords_sra <- just_coords_sra[!is.na(just_coords_sra$longitude),]
+just_coords_sra <- just_coords_sra[!is.na(just_coords_sra$latitude),]
+just_coords_sra <- just_coords_sra[!is.na(just_coords_sra$longitude),]
 #https://r-spatial.github.io/sf/reference/sf.html
 sample_st<- st_as_sf(just_coords_sra, coords = c("longitude", "latitude"),
           crs=4326)
@@ -38,7 +40,7 @@ r <- rbind(c(-180, -90), c(-181, -90), c(-181, 90), c(-180, 90), c(-180, -90)) %
   st_sfc %>%
   st_set_crs(.,4326)
 
-#i <- st_intersection(poly_sra_0, r)
+i <- st_intersection(poly_sra_0, r)
 ########sra resolution 0#########
 cells_sra_0 <- point_to_cell(sample_st, res=0, simple=F) #data frame
 #write.csv(cells_sra_0, "~/../../scratch/alexa/pres_rev_figs/03_figures/03_geoloc/hex/sra_0_full.csv")
@@ -50,18 +52,18 @@ cells_w_count_sra_0<- count(cells_sra_0, h3_resolution_0, sort = TRUE) #get coun
 #poly_sra <- unlist(cells_w_count_sra, use.names = T) #not sure if this is needed?
 poly_sra_0 <- cell_to_polygon(cells_w_count_sra_0, simple =F)
 i <- st_intersection(poly_sra_0, r)
-plot(i, add = F, lty = 2, col = "red")
+#plot(i, add = F, lty = 2, col = "red")
 
 h3_to_drop <- i$h3_address
 cropped_poly_sra_0<- poly_sra_0[!poly_sra_0$h3_address %in% h3_to_drop,]
 plot(poly_sra_0)
 plot(cropped_poly_sra_0)
+#in cases where the grid cells cross the 180th meridian, this cropping removes image artifacts
 
 ########resolution 1#########
 cells_sra_1 <- point_to_cell(sample_st, res=1, simple=F) #data frame
-write.csv(cells_sra_1, "~/../../scratch/alexa/pres_rev_figs/03_figures/03_geoloc/hex/sra_1_full.csv")
-rm(cells_sra_1)
-#cells_sra %>% count(scrubbed_species_binomial, h3_resolution_2, sort = TRUE) 
+#write.csv(cells_sra_1, "~/../../scratch/alexa/pres_rev_figs/03_figures/03_geoloc/hex/sra_1_full.csv")
+#rm(cells_sra_1)
 cells_sra_1 <- count(cells_sra_1, scrubbed_species_binomial, h3_resolution_1, sort = TRUE) 
 cells_w_count_sra_1<- count(cells_sra_1, h3_resolution_1, sort = TRUE) #get count for each hexagon
 #poly_sra <- unlist(cells_w_count_sra, use.names = T) #not sure if this is needed?
@@ -72,31 +74,17 @@ h3_to_drop <- i$h3_address
 cropped_poly_sra_1<- poly_sra_1[!poly_sra_1$h3_address %in% h3_to_drop,]
 plot(poly_sra_1)
 plot(cropped_poly_sra_1)
-########res 2########
+########resolution 2########
 cells_sra_2 <- point_to_cell(sample_st, res=2, simple=F) #data frame
-write.csv(cells_sra_2, "~/../../scratch/alexa/pres_rev_figs/03_figures/03_geoloc/hex/sra_2_full.csv")
-rm(list = ls())
+#write.csv(cells_sra_2, "~/../../scratch/alexa/pres_rev_figs/03_figures/03_geoloc/hex/sra_2_full.csv")
+#rm(list = ls())
 
-cells_sra_2 <- count(cells_sra, scrubbed_species_binomial, h3_resolution_2, sort = TRUE) 
-cells_w_count_sra<- count(cells_sra, h3_resolution_2, sort = TRUE) #get count for each hexagon
+cells_sra_2 <- count(cells_sra_2, scrubbed_species_binomial, h3_resolution_2, sort = TRUE) 
+cells_w_count_sra_2<- count(cells_sra_2, h3_resolution_2, sort = TRUE) #get count for each hexagon
 #poly_sra <- unlist(cells_w_count_sra, use.names = T) #not sure if this is needed?
-poly_sra <- cell_to_polygon(cells_w_count_sra, simple =F)
+poly_sra_2 <- cell_to_polygon(cells_w_count_sra, simple =F)
 
-write.csv(cells_sra_1, "~/../../scratch/alexa/pres_rev_figs/03_figures/03_geoloc/hex/sra_1_full.csv")
-rm(cells_sra_1)
-#cells_sra %>% count(scrubbed_species_binomial, h3_resolution_2, sort = TRUE) 
-cells_sra_1 <- count(cells_sra_1, scrubbed_species_binomial, h3_resolution_1, sort = TRUE) 
-cells_w_count_sra_1<- count(cells_sra_1, h3_resolution_1, sort = TRUE) #get count for each hexagon
-#poly_sra <- unlist(cells_w_count_sra, use.names = T) #not sure if this is needed?
-poly_sra_1 <- cell_to_polygon(cells_w_count_sra_1, simple =F)
-#plot(i, add = F, lty = 2, col = "red")
-i <- st_intersection(poly_sra_1, r)
-h3_to_drop <- i$h3_address
-cropped_poly_sra_1<- poly_sra_1[!poly_sra_1$h3_address %in% h3_to_drop,]
-plot(poly_sra_1)
-plot(cropped_poly_sra_1)
-
-#########plot_rich from previous pipeline#########
+#########plot_rich from full step#########
 plot_rich_hexes <- function(rich, name){ #richness points to plot, name to save as
   ggplot() +
     scale_fill_viridis(option="mako", direction=-1,na.value = 'white')+ #figure out later
@@ -119,12 +107,13 @@ plot_rich_hexes <- function(rich, name){ #richness points to plot, name to save 
   st_write(rich, paste(name, ".shp", sep=""))
  # writeRaster(rich, paste(name, ".shp", sep=""))
 }
+setwd("Documents/GitHub/preservation-review-2024/05_image_creation_hex/output_oryza_example/")
 plot_rich_hexes(poly_sra_0, "sra_res_0")
-plot_rich_hexes(cropped_poly_sra_0, "sra_res_0_cropped")
+#plot_rich_hexes(cropped_poly_sra_0, "sra_res_0_cropped")
 plot_rich_hexes(poly_sra_1, "sra_res_1")
-plot_rich_hexes(cropped_poly_sra_1, "sra_res_1_cropped")
-plot_rich_hexes(poly_sra_3, "sra_res_3")
-plot_rich_hexes(cropped_poly_sra_3, "sra_res_3_cropped")
+#plot_rich_hexes(cropped_poly_sra_1, "sra_res_1_cropped")
+plot_rich_hexes(poly_sra_2, "sra_res_2")
+#plot_rich_hexes(cropped_poly_sra_2, "sra_res_2_cropped")
 
 
 ##########whole setup#########
